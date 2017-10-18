cee627693ebe3474f85eb48958ea47a963c48236
zl2290103097 <2290103097@qq.com>
2017-10-18 11:36:32 +0800

    1018

--- ZLSOURCEFILE_ALL/ALLSourceFile/CommonZl/Zhou02/KZPhotoManager.h
+++ ZLSOURCEFILE_ALL/ALLSourceFile/CommonZl/Zhou02/KZPhotoManager.h
@@ -8,18 +8,23 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <AVFoundation/AVFoundation.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface KZPhotoManager : NSObject
 {
     UIImagePickerController *pickerController;
     void (^saveImageCallBack)(UIImage *image);
+    void (^saveVideoURLCallBack)(NSURL *videoURL);
+    void (^(saveVideoDataCallBack))(NSData * videoData);
+    void (^(saveVideoFirstImageCallBack))(UIImage *videoFirstImage);
     
 }
 
 
 + (void)getImage:(void (^)(UIImage *image))img showIn:(UIViewController *)controller AndActionTitle:(NSString *)title;
 
-
-
++ (void)getVideo:(void (^)(NSURL * videoURL))vURL withData:(void (^)(NSData * videoData)) vData withFirstImage:(void (^)(UIImage *firstimage))fristImage showIn:(UIViewController *) controller AndActionTitle:(NSString *)title;
 
 @end
